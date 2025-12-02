66664ba63e350b89ee6562c707b8cb4d50561ae0
Jan <meissl.jan@gmail.com>
2025-12-02 20:55:18 +0000

    fix volumes

--- web1/scripts/nasa/apod.sh
+++ web1/scripts/nasa/apod.sh
@@ -3,7 +3,7 @@
 # Author: hans
 # Datum: Mon Oct 27 13:25:51 UTC 2025
 
-picture=/var/log/app/apod.jpg
+picture=/opt/app/web/apod.jpg
 
 
 url=$(curl -s "https://api.nasa.gov/planetary/apod?api_key=YoP0mZxujKOeBRem9Szf4BhVh0Qs96DeU7Guzxyy" | jq -r '.url')
